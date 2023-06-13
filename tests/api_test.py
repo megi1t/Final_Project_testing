a03c211c457336aa441bd6bc96539d87accbcf27
Megi Tkeshelashvili <98535199+megi1t@users.noreply.github.com>
2023-06-13 13:38:01 +0400

    Updated ci.yaml

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,17 +1,14 @@
+import pytest
 import requests
 import unittest
-import pytest
-import allure_commons._allure
-
+from allure_commons._allure import title, description
 
 BASE_URL = "http://restapi.adequateshop.com/api"
 
 # Tests for Registration and login
-@pytest.mark.allure
 class LoginAndRegistrationTests(unittest.TestCase):
-    @pytest.mark.test
-    @allure_commons._allure.title("Successful Registration")
-    @allure_commons._allure.description("Test successful registration of a user")
+    @title("Successful Registration")
+    @description("Test successful registration of a user")
     def test_successful_registration(self):
         payload = {"name": "Test",
                    "email": "testtest001@test.com", "password": "123GGG"}
@@ -19,9 +16,8 @@ class LoginAndRegistrationTests(unittest.TestCase):
             f"{BASE_URL}/AuthAccount/Registration", json=payload)
         self.assertEqual(response.status_code, 200)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsucsessfull Registration")
-    @allure_commons._allure.description("Test registering user with password that is shorter than 6 characters")
+    @title("Unsucsessfull Registration")
+    @description("Test registering user with password that is shorter than 6 characters")
     def test_unsuccessful_registration_short_password(self):
         payload = {"name": "TestUser",
                    "email": "someuser@test.com", "password": "123GG"}
@@ -29,9 +25,8 @@ class LoginAndRegistrationTests(unittest.TestCase):
             f"{BASE_URL}/AuthAccount/Registration", json=payload)
         self.assertEqual(response.status_code, 400)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsucsessfull Registration")
-    @allure_commons._allure.description("Test re-registering already registered user")
+    @title("Unsucsessfull Registration")
+    @description("Test re-registering already registered user")
     def test_unsuccessful_registration_registered_user(self):
         payload = {"name": "TestUser1",
                    "email": "random_test_user2@test.com", "password": "123GGH"}
@@ -43,17 +38,15 @@ class LoginAndRegistrationTests(unittest.TestCase):
         expected_message = "The email address you have entered is already registered"
         self.assertEqual(error_message, expected_message)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Successful Login")
-    @allure_commons._allure.description("Test successful login with valid email and password")
+    @title("Successful Login")
+    @description("Test successful login with valid email and password")
     def test_successful_login(self):
         payload = {"email": "valid_megi@test.com", "password": "123GGG"}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
         self.assertEqual(response.status_code, 200)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Successful Login")
-    @allure_commons._allure.description("Test successful login with valid email and password")
+    @title("Successful Login")
+    @description("Test successful login with valid email and password")
     def test_unsuccessful_login_incorrect_email(self):
         payload = {"email": "invalid_megi@test.com", "password": "123GGG"}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
@@ -63,9 +56,8 @@ class LoginAndRegistrationTests(unittest.TestCase):
         expected_message = "invalid username or password"
         self.assertEqual(error_message, expected_message)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsuccessful Login")
-    @allure_commons._allure.description("Test unsuccessful login with valid email and incorrect password")
+    @title("Unsuccessful Login")
+    @description("Test unsuccessful login with valid email and incorrect password")
     def test_unsuccessful_login_incorrect_password(self):
         payload = {"email": "valid_megi@test.com", "password": "123GGGG"}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
@@ -75,9 +67,8 @@ class LoginAndRegistrationTests(unittest.TestCase):
         expected_message = "invalid username or password"
         self.assertEqual(error_message, expected_message)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsuccessful Login")
-    @allure_commons._allure.description("Test unsuccessful login with incorrect credentials")
+    @title("Unsuccessful Login")
+    @description("Test unsuccessful login with incorrect credentials")
     def test_unsuccessful_login_incorrect_credentials(self):
         payload = {"email": "invalid_megi@test.com", "password": "123GGGG"}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
@@ -87,26 +78,73 @@ class LoginAndRegistrationTests(unittest.TestCase):
         expected_message = "invalid username or password"
         self.assertEqual(error_message, expected_message)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsuccessful Login")
-    @allure_commons._allure.description("Test unsuccessful login with empty credentials")
+    @title("Unsuccessful Login")
+    @description("Test unsuccessful login with empty credentials")
     def test_unsuccessful_login_empty_credentials(self):
         payload = {"email": "", "password": ""}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
         self.assertEqual(response.status_code, 400)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsuccessful Login")
-    @allure_commons._allure.description("Test unsuccessful login with empty email")
+    @title("Unsuccessful Login")
+    @description("Test unsuccessful login with empty email")
     def test_unsuccessful_login_empty_username(self):
         payload = {"email": "", "password": "123GGG"}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
         self.assertEqual(response.status_code, 400)
 
-    @pytest.mark.test
-    @allure_commons._allure.title("Unsuccessful Login")
-    @allure_commons._allure.description("Test unsuccessful login with empty password")
+    @title("Unsuccessful Login")
+    @description("Test unsuccessful login with empty password")
     def test_unsuccessful_login_empty_password(self):
         payload = {"email": "valid_megi@test.com", "password": ""}
         response = requests.post(f"{BASE_URL}/AuthAccount/Login", json=payload)
         self.assertEqual(response.status_code, 400)
+
+
+# Tests for Get Tourist Endpoint
+class GetTouristEndpointTests(unittest.TestCase):
+    @title("Successfully create and retrieve a specific tourist")
+    @description("Test successfully creates a tourist and retrieves created tourist by id")
+    def test_get_tourist_by_id(self):
+        payload = {
+            "id": 17,
+            "tourist_name": "James Bond",
+            "tourist_email": "jb5@test.com",
+            "tourist_location": "London",
+            "createdat": "2023-06-12T18:59:56.868Z"
+        }
+        create_response = requests.post(f"{BASE_URL}/Tourist", json=payload)
+        assert create_response.status_code == 201
+        # retrieve tourist id
+        tourist_id = create_response.json()['id']
+        get_response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
+        assert get_response.status_code == 200
+        print(get_response.json())
+
+    @title("Successfully create a tourist and retrieve it with wrong id")
+    @description("Test successfully creates a tourist but retrieves tourist by wrong id")
+    def test_get_tourist_by_wrongid(self):
+        payload = {
+            "id": 19,
+            "tourist_name": "James Bond 007",
+            "tourist_email": "jb6@test.com",
+            "tourist_location": "London",
+            "createdat": "2023-06-12T18:59:56.868Z"
+        }
+        create_response = requests.post(f"{BASE_URL}/Tourist", json=payload)
+        assert create_response.status_code == 201
+        # retrieve tourist id
+        tourist_id = 1
+        get_response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
+        assert get_response.status_code == 404
+
+    @title("Unsuccessfully retrieve a tourist with invalid id")
+    @description("Test unsuccessfully retrieves a tourist with invalid id")
+    def test_get_tourist_by_invalidid(self):
+        tourist_id = "djfk2"
+        response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
+        assert response.status_code == 400
+        json_data = response.json()
+        error_message = json_data["Message"]
+        expected_message = "The request is invalid."
+        self.assertEqual(error_message, expected_message)
+
--- tests/get_test.py
+++ tests/get_test.py
@@ -1,58 +1,6 @@
 import requests
 import unittest
-import pytest
 from allure_commons._allure import title, description
 
 BASE_URL = "http://restapi.adequateshop.com/api"
 
-# Tests for Get Tourist Endpoint
-@pytest.mark.allure
-class GetTouristEndpointTests(unittest.TestCase):
-    @pytest.mark.test
-    @title("Successfully create and retrieve a specific tourist")
-    @description("Test successfully creates a tourist and retrieves created tourist by id")
-    def test_get_tourist_by_id(self):
-        payload = {
-            "id": 17,
-            "tourist_name": "James Bond",
-            "tourist_email": "jb0@test.com",
-            "tourist_location": "London",
-            "createdat": "2023-06-12T18:59:56.868Z"
-        }
-        create_response = requests.post(f"{BASE_URL}/Tourist", json=payload)
-        assert create_response.status_code == 201
-        # retrieve tourist id
-        tourist_id = create_response.json()['id']
-        get_response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
-        assert get_response.status_code == 200
-        print(get_response.json())
-
-    @pytest.mark.test
-    @title("Successfully create a tourist and retrieve it with wrong id")
-    @description("Test successfully creates a tourist but retrieves tourist by wrong id")
-    def test_get_tourist_by_wrongid(self):
-        payload = {
-            "id": 19,
-            "tourist_name": "James Bond 007",
-            "tourist_email": "jb3@test.com",
-            "tourist_location": "London",
-            "createdat": "2023-06-12T18:59:56.868Z"
-        }
-        create_response = requests.post(f"{BASE_URL}/Tourist", json=payload)
-        assert create_response.status_code == 201
-        # retrieve tourist id
-        tourist_id = 1
-        get_response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
-        assert get_response.status_code == 404
-
-    @pytest.mark.test
-    @title("Unsuccessfully retrieve a tourist with invalid id")
-    @description("Test unsuccessfully retrieves a tourist with invalid id")
-    def test_get_tourist_by_invalidid(self):
-        tourist_id = "djfk2"
-        response = requests.get(f"{BASE_URL}/Tourist/{tourist_id}")
-        assert response.status_code == 400
-        json_data = response.json()
-        error_message = json_data["Message"]
-        expected_message = "The request is invalid."
-        self.assertEqual(error_message, expected_message)
